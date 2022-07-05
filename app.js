baa81333aec0e23746e2a0c07df40d7f45649dd3
Сергей Смирнов <sergejsmirnov@MacBook-Air-Sergej.local>
2022-07-05 12:57:16 +0300

    dev2

--- app.js
+++ app.js
@@ -11,12 +11,12 @@ const { NotFoundError } = require('./errors/index');
 const { router } = require('./routes');
 
 const app = express();
-const { PORT = 3001 } = process.env;
+const { PORT = 3002 } = process.env;
 app.use(express.json());
 app.use(cors({
-  origin: true,
-  exposedHeaders: '*',
-  credentials: true,
+ origin: true,
+ exposedHeaders: '*',
+ credentials: true,
 }));
 mongoose.connect(NODE_ENV === 'production' ? DB_CONNECTION_STRING : 'mongodb://localhost:27017/bitfilmsdb', {
   useNewUrlParser: true,
